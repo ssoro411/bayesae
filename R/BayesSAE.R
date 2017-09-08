bfd2aaea441c9fb0e3b49d39971fae5750074376
HeeCheol Chung <heecheol.chung25@uga.edu>
2017-09-08 17:47:46 -0400

    4th commit

--- R/BayesSAE.R
+++ R/BayesSAE.R
@@ -4,8 +4,6 @@ rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
 
-basepath <- file.path(Sys.getenv("PROJECTS"), "bayesae")
-stanpath <- file.path(basepath, "R")
 
 
 
@@ -22,6 +20,129 @@ setGeneric("getEstimates",
            def = function(object){standardGeneric("getEstimates")})
 setMethod("getEstimates", signature = "stanfit.sae",
           definition = function(object){object@estimates})
+
+
+#########################################################################
+##  Function specifying selected model.
+#########################################################################
+Model = function(model) {
+if(model=="FH"){
+m <- "
+data {
+  int<lower=0>       m;    // Number of small areas
+  int<lower=0>       p;    // Number of auxiliary variables
+  real            y[m];    // Direct Estimate
+  real<lower=0> sDi[m];    // Square root of sampling variances
+  vector[p]        X[m];    // Auxiliary variable
+}
+parameters {
+  real<lower=0>     sigma;       // Scale parameter
+  vector[p]          beta;       // Regression parameter
+  vector[m]         theta;       // Area characteristics
+}
+transformed parameters {
+  vector[m] mu;
+    for( i in 1:m)
+    mu[i] = X[i]'*beta;
+}
+model {
+  for( i in 1:m)
+  theta[i] ~ normal(mu[i], sigma);
+  for( i in 1:m)
+  y[i]     ~ normal(theta[i], sDi[i]);
+}
+generated quantities{
+vector[m] log_lik;
+for( i in 1:m)
+log_lik[i] = normal_lpdf(y[i]|theta[i],sDi[i]);
+}
+
+"} else if (model == "SAR") {
+#########################################################################
+##  Stan Model : SAR
+#########################################################################
+
+m <- "
+data {
+  int<lower=0>     m;        // Number of small areas
+  int<lower=0>     p;        // Number of auxiliary variables
+  real           y[m];       // Direct Estimate
+  real<lower=0> sDi[m];      // Sampling Error
+  matrix[m,m]   W;           // Spatial matrix
+  matrix[m,p]   X;           // Auxiliary variable
+  matrix[m,m]   I;           // Identity matrix
+}
+parameters {
+  real<lower=-0.99999, upper=0.99999> rho;   // Spatial parameter 
+  real<lower=0>  sigma_sq;                   // Scale parameter
+  vector[7]          beta;                   // Regression parameter
+  vector[m]         theta;                   // Area characteristics
+}
+transformed parameters {
+  vector[m] mu;
+  cov_matrix[m] G;
+  mu = X*beta;
+  G = (1/sigma_sq)*( (I-rho*(W))*(I-rho*(W'))  ) ;// SAR precision matrix
+}
+model {
+  theta    ~ multi_normal_prec(mu, G);
+  for( i in 1:m)
+  y[i]     ~ normal(theta[i], sDi[i]);
+}
+generated quantities{
+    vector[m] log_lik;
+    for( i in 1:m)
+    log_lik[i] = normal_lpdf(y[i]|theta[i],sDi[i]);
+}
+
+"
+} else {
+#########################################################################
+##  Stan Model : SAR
+#########################################################################
+
+m <- "
+data {
+  int<lower=0>     m;        // Number of small areas
+  int<lower=0>     p;        // Number of auxiliary variables
+  real           y[m];       // Direct estimate
+  real<lower=0> sDi[m];      // Sampling standard deviation
+  matrix[m,m]   W;           // Spatial matrix
+  matrix[m,p]   X;           // Auxiliary variable
+  matrix[m,m]   I;           // Identity matrix
+  real rupper;                   // Upper bound of rho
+  real rlower;                   // Lower bound of rho
+}
+parameters {
+  real<lower=rlower, upper=rupper> rho;  // Spatial parameter
+  real<lower=0>  sigma_sq;       // Scale parameter
+  vector[p]          beta;       // Regression parameter
+  vector[m]         theta;       // Characteristic of areas
+}
+transformed parameters {
+  vector[m] mu;
+  cov_matrix[m] G;
+  mu = X*beta;
+  G = (1/sigma_sq)*( (I-rho*(W)) ) ; // CAR precision matrix
+}
+model {
+  theta    ~ multi_normal_prec(mu, G);
+  for( i in 1:m)
+  y[i]     ~ normal(theta[i], sDi[i]);
+}
+generated quantities{
+    vector[m] log_lik;
+    for( i in 1:m)
+    log_lik[i] = normal_lpdf(y[i]|theta[i],sDi[i]);
+}
+
+"
+}
+return(m)
+}
+
+
+
 
 
 
@@ -46,12 +167,20 @@ BayesSAE <- function(formula, data = NULL , Di = NULL, domain = NULL,
     dat <- list(m=dim(X)[1], p=dim(X)[2], y=Y, X=X, sDi= sqrt(Di), W=WSAR, I=I, rupper=range[2],rlower=range[1] )
      }
 
-    stanfit <- stan(file = file.path(stanpath, model_name), data = dat,
-                    iter = iter, pars = c("sigma_sq", "beta", "theta",
-                                          "log_lik"),
+    # stanfit <- stan(file = file.path(stanpath, model_name), data = dat,
+    #                 iter = iter, pars = c("sigma_sq", "beta", "theta",
+    #                                       "log_lik"),
+    #                 warmup = warmup, chains = chains,
+    #                open_progress=open.progress,
+    #                control = list(max_treedepth=15, adapt_delta = 0.99))
+
+    stanfit <- stan(model_code = Model(model), ,model_name = model,
+                    data = dat, iter = iter, 
+                    pars = c("sigma_sq", "beta", "theta","log_lik"),
                     warmup = warmup, chains = chains,
-                   open_progress=open.progress,
-                   control = list(max_treedepth=15, adapt_delta = 0.99))
+                    open_progress=open.progress,
+                    control = list(max_treedepth=15, adapt_delta = 0.99))
+    
     theta.smpl <- extract(stanfit, pars = "theta", permuted = FALSE)
     if(logit.trans) theta.smpl <- expit(theta.smpl)
     posterior.summary <- data.frame(domain = rownames(data),
