4175d0d6236e62ffacd7c47f800a00180c52112d
HeeCheol Chung <heecheol.chung25@uga.edu>
2017-09-12 20:18:29 -0400

    BM Estimator

--- R/BenchMarking.R
+++ R/BenchMarking.R
@@ -1,13 +1,25 @@
-library(roxygen2)
-roxygen2::roxygenise()
+#' Benchmarked Bayes Estimator
+#'
+#' Constrained Bayes estimator which minimizes posterior weighted squared error loss.
+#' @param theta_b Bayes estimator.
+#' @param w Given weight vector.
+#' @param t Pre-specified number.
+#' @param phi
+#' @param lambda
+#' @return T square of the input
+#' @export
+#' @references Datta, Gauri Sankar, et al. "Bayesian benchmarking with applications to small area estimation." Test 20.3 (2011): 574-588.
+
 ## Benchmarked Bayes Estimator
 
-BM = function(theta_b, t, w, phi, lambda=NA){
+BM = function(theta_b, w, t, phi, lambda=NA){
 r = w/phi
 s = sum(w*r)
+
 if( is.na(lambda) ){
-theta_BM = theta_b + ( (s )^(-1) )*(t - sum(w*theta_b) )*r
-} else{
-theta_BM = theta_b + ( (s + lambda^(-1))^(-1) )*(t - sum(w*theta_b) )*r}
+  theta_BM = theta_b + ( (s )^(-1) )*(t - sum(w*theta_b) )*r
+  } else{
+  theta_BM = theta_b + ( (s + lambda^(-1))^(-1) )*(t - sum(w*theta_b) )*r}
+
 return(theta_BM)
 }
