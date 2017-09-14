fcebd7d576502628d5c65ae8ea041b280fb3aee1
HeeCheol Chung <heecheol.chung25@uga.edu>
2017-09-14 15:29:58 -0400

    ref

--- R/BenchMarking.R
+++ R/BenchMarking.R
@@ -5,7 +5,7 @@
 #' @param w Weight vector associates with the benchmark.
 #' @param t Pre-specified number.
 #' @param phi Weight vector associates with weighted squared sum of square loss.
-#' @param lambda Penalty parameter. When it is NULL, lambda = infinity and it will give exact
+#' @param lambda Penalty parameter. When it is NULL, lambda = infinity and it will give exact benchmark.
 #' @return Resulting Benchmarked Bayes Estimator.
 #' @export
 #' @references
